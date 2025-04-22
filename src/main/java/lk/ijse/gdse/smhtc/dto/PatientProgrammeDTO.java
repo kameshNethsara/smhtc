3614eee8b12033b906e1e903da42fd90f3dc4cab
kameshNethsara <kamesh317nethsara@gmail.com>
2025-04-22 21:01:41 +0530

    create PatientProgrammeDTO

---
+++ src/main/java/lk/ijse/gdse/smhtc/dto/PatientProgrammeDTO.java
@@ -0,0 +1,17 @@
+package lk.ijse.gdse.smhtc.dto;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class PatientProgrammeDTO {
+    private String patientId;
+    private String patientName;
+    private String programId;
+    private String programName;
+    private String paymentId;
+    private String registrationDate;
+}
